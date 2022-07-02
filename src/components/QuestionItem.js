ff02935739d0cf6ecc28e07399f683d24bdfd7b6
Michelle Lau <meeshklau@gmail.com>
2022-07-02 13:03:52 -1000

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, questions, setQuestions, questionNumber }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  // Long form:
+  // const id = question.id
+  // const prompt = question.prompt
+  // const answers = question.answers
+  // const correctIndex = question.correctIndex
+
+  const thisQuestionURL = "http://localhost:4000/questions/" + id
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +17,47 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const deleteQuestion = () => {
+    fetch(thisQuestionURL, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      setQuestions(questions.filter(eachQuestion => {
+        return eachQuestion.id !== id
+      }))
+    })
+  }
+
+  const updateQuestionWithCorrectIndexAPI = (event) => {
+    fetch(thisQuestionURL, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        correctIndex: event.target.value
+      })
+    })
+    .then(response => response.json())
+    .then(json => updateQuestionWithCorrectIndexState(json))
+  }
+
+  const updateQuestionWithCorrectIndexState = (currentQuestion) => {
+    let currentQuestionIndex = questions.findIndex(question => question.id === currentQuestion.id)
+    let spreadQuestions = [...questions]
+    spreadQuestions.splice(currentQuestionIndex, 1, currentQuestion)
+    setQuestions(spreadQuestions)
+  }
+
   return (
     <li>
-      <h4>Question {id}</h4>
+      <h4>Question {questionNumber}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={updateQuestionWithCorrectIndexAPI} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteQuestion}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, setQuestions}) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(response => response.json())
+    .then(json => setQuestions(json))
+  }, [])
+
+  const renderQuestionItemFactory = () => {
+    if(!!questions) {
+      let questionNumber = 0;
+      return questions.map(currentQuestion => {
+        questionNumber += 1;
+        return (
+          <QuestionItem 
+            key={currentQuestion.id} 
+            questionNumber={questionNumber} 
+            question={currentQuestion} 
+            questions={questions} 
+            setQuestions={setQuestions} 
+          />
+        )
+      })
+    }
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{renderQuestionItemFactory()}</ul>
     </section>
   );
 }
